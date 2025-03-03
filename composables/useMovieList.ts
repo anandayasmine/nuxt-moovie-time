ab83e30031526d36718bda323e76b916f9bd7cac
Ananda Yasmine <>
2025-03-03 23:49:16 +0700

    Commit: - add try catch

--- composables/useMovieList.ts
+++ composables/useMovieList.ts
@@ -1,19 +1,26 @@
 export const useMovieList = async (props?: { query?: any }) => {
+  try{
 
-  const query = props?.query
-  const search = props?.query?.search || 'movie'
-  const config = useRuntimeConfig();
-  const apiKey = config.public.API_KEY;
-
-  const url = new URL("http://www.omdbapi.com?s=" + search + "&apikey=" + apiKey);
-  const params = new URLSearchParams(url.search);
-  if (query) {
-    Object.keys(query)?.forEach(key => {
-      params.append(key, query[key]);
-    })
+    const query = props?.query
+    const search = props?.query?.search || 'movie'
+    const config = useRuntimeConfig();
+    const apiKey = config.public.API_KEY;
+  
+    const url = new URL("http://www.omdbapi.com?s=" + search + "&apikey=" + apiKey);
+    const params = new URLSearchParams(url.search);
+    if (query) {
+      Object.keys(query)?.forEach(key => {
+        params.append(key, query[key]);
+      })
+    }
+    const response: any = await $fetch(url.origin + '?' + params.toString());
+  
+    return response?.Search;
   }
-  const response: any = await $fetch(url.origin + '?' + params.toString());
+  catch(err){
 
-  return response?.Search;
+    return null
+
+  }
 
 }
